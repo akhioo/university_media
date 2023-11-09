988d71c926977f5a6d01aa5a1fb4cb22e1918be5
Akzhol Aidossuly <aidos.akzhol05@gmail.com>
2023-11-09 20:46:16 +0600

    feat: added controllers for authorization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { AuthService } from "./services/auth.service";
+import { SignUpDTO } from "./dto/signUp.dto";
+import { LoginDTO } from "./dto/login.dto";
 
 @Controller('users')
-export class UsersController {}
+@UsePipes(new ValidationPipe())
+export class UsersController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(201)
+  @Post('signup')
+  async onUserSignUp(@Body() signUpDTO: SignUpDTO) {
+    return await this.authService.signUp(signUpDTO)
+  }
+
+  @HttpCode(202)
+  @Post('login')
+  async onUserLogin(@Body() loginDTO: LoginDTO) {
+    return await this.authService.logIn(loginDTO)
+  }
+}
+

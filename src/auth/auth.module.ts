867454c1367cfaa1d796ff6d0190613e32d82a59
Akzhol Aidossuly <aidos.akzhol05@gmail.com>
2023-11-15 08:04:06 +0600

    fix: debugged the project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { UsersService } from '../users/users.service';
 import { AuthService } from './services';
+import { UsersModule } from '../users/users.module';
+import { LoginAuditService } from "./services";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { LoginRecordEntity } from "./entities";
 
 @Module({
+  imports: [UsersModule, TypeOrmModule.forFeature([LoginRecordEntity])],
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [],
+  providers: [AuthService, LoginAuditService],
 })
 export class AuthModule {}
